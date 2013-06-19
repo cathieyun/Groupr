f5361d84fdea37f0e5d07de7442613bf89857db1
Cathie <cathieyun@gmail.com>
2013-06-18 21:11:23 -0700

    Setting up the backbone, woohoo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groupr::Application.config.secret_token = '88ec08124601645b88646fadda3e610486329086f6526f64122e8eb497abb9bdc8c97fbe07c8f9c230ec0d7a4b9886ca386caac57ef71844b7c509b6e106d065'
